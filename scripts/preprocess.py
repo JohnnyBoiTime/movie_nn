ed6d0387db04a0d6c24275d5698624524bdd29c9
JohnnyBoiTime <chanky789@icloud.com>
2025-06-12 15:30:27 -0700

    Finished preprocess code

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 # Load data sets from the ml csv
-ratings = pd.read_csv('/data/rawData/ratings.csv')
-movies = pd.read_csv('/data/rawData/movies.csv')
+ratings = pd.read_csv('./data/rawData/ratings.csv')
+movies = pd.read_csv('./data/rawData/movies.csv')
 
 # drop dupes
 ratings = ratings.drop_duplicates(subset=['userId', "movieId"])
@@ -15,25 +15,25 @@ ratings = ratings.drop_duplicates(subset=['userId', "movieId"])
 numUsers = ratings.userId.value_counts()
 numMovies = ratings.movieId.value_counts()
 
+# Keep users and movies that have at least 5 ratings
 signifUsers = numUsers[numUsers >= 5].index
 signifMovies = numMovies[numMovies >= 5].index
 
 # Filter out the ratings so they only keep the users of our choosing
 ratings = (
     ratings[ratings.userId.isin(signifUsers) & 
-            ratings.movieId.isin(signifMovies).reset_index(drop=True)]
+            ratings.movieId.isin(signifMovies)]
 )
 
-# Tokenize words into something the neural network can understand.
 # LabelEncoder converts the categorical labels into integers
 userLabels = LabelEncoder()
 movieLabels = LabelEncoder()
 
 # We then fit the labels to the users and movie IDs
-ratings["userIndex"] = userLabels.fit_transform(ratings.userID)
+ratings["userIndex"] = userLabels.fit_transform(ratings.userId)
 ratings["movieIndex"] = movieLabels.fit_transform(ratings.movieId)
 
-# Make the above embeddings unique
+# Count unique indeces for the users and movies
 users = ratings.userIndex.nunique()
 movies = ratings.movieIndex.nunique()
 
@@ -48,7 +48,7 @@ trainingDataFrame, testingDataFrame = train_test_split(
 # 0.1 = 10% data will be put into validation, 90% put in training/fitting
 # 42 = random seed so we get same split
 trainingDataFrame, validationDataFrame = train_test_split(
-    ratings, test_size=0.1, random_state =42
+    trainingDataFrame, test_size=0.1, random_state =42
 )
 
 # Save processed data to csv files:
@@ -56,4 +56,9 @@ trainingDataFrame.to_csv(os.path.join("data", "processed", "training.csv"), inde
 validationDataFrame.to_csv(os.path.join("data", "processed", "validation.csv"), index=False)
 testingDataFrame.to_csv(os.path.join("data", "processed", "testing.csv"), index=False)
 
+# Save label encoders
+with open(os.path.join("data", "processed", "userEncoder.pkl"), "wb") as f:
+    pickle.dump(userLabels, f)
 
+with open(os.path.join("data", "processed", "movieEncoder.pkl"), "wb") as f:
+    pickle.dump(movieLabels, f)
