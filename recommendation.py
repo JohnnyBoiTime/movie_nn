5767a036f6de89705229a62d6146b5253c05a4c7
JohnnyBoiTime <chanky789@icloud.com>
2025-06-17 21:27:06 -0700

    It works but is inaccurate

---
+++ recommendation.py
@@ -0,0 +1,70 @@
+import torch
+import pickle
+import pandas as pd
+import os
+import torch.nn.functional as F
+from model import MovieRecModel
+
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+
+projectRoot = os.path.abspath(os.path.join(currentDirectory, os.pardir))
+
+rawDataDirectory = os.path.join(projectRoot, "movie_nn", "data", "rawData")
+
+pickleDirectory = os.path.join(projectRoot, "movie_nn", "data", "processed", "movieEncoder.pkl")
+
+trainedModel = os.path.join(projectRoot, "movie_nn", "models", "movierec.pth")
+
+movies = pd.read_csv(os.path.join(rawDataDirectory, "movies.csv"))
+
+# Make it so user can simply type a string to get similar movies
+movieIdToTitle = dict(zip(movies.movieId, movies.title))
+titleToMovieId = dict(zip(movies.title, movies.movieId))
+
+# Grab and load the pickle
+with open(pickleDirectory, "rb") as f:
+    movieEncoder = pickle.load(f)
+
+# get the indexes from the encoder
+classes = movieEncoder.classes_    
+indexToMovie = {i: classes[i] for i in range(len(classes))}
+
+# params set for model
+numUsers = 610
+numMovies = len(classes)
+embeddingSize = 50
+
+
+gpuFound = torch.device("cuda" if torch.cuda.is_available() else "cpu") # check if GPU is available
+model = MovieRecModel(numUsers, numMovies, embeddingSize).to(gpuFound) # train on GPU, default CPU
+
+# Load trained model
+model.load_state_dict(torch.load(trainedModel, map_location=gpuFound))
+model.eval()
+
+movieEmbedds = model.movieEmbedding.weight.data.cpu()
+
+# Test the model
+def recommendationSystemTest(movieTitle, k=5):
+
+    # Make sure movie title is valid!
+    if movieTitle not in titleToMovieId:
+        raise KeyError("Movie does not exist in this model or incorrect input format")
+    
+    # convert title to its corresponding ID in the dictionary
+    movieID = titleToMovieId.get(movieTitle)
+    index = classes[movieID]
+    if index is None:
+        raise KeyError(f"Movie ID {movieTitle} is not in the pickle encoder")
+    
+    # It is confirmed to exist, now find similar movies!
+    queryVector = movieEmbedds[index].unsqueeze(0)
+    calcSimilarity = F.cosine_similarity(queryVector, movieEmbedds)
+    calcSimilarity[index] = -1.0 # Don't recommend the same movie
+    topKValues, topKIndexes = torch.topk(calcSimilarity, k)
+
+    # We found the simular movies
+    return [(movieIdToTitle.get(indexToMovie[index.item()]), topKValues[j].item())
+        for j, index in enumerate(topKIndexes)]
+
+
---
+++ testModel.py
@@ -0,0 +1,10 @@
+from recommendation import recommendationSystemTest
+
+if __name__ == "__main__":
+
+    # From the name of the movie + year, find similar movies contained in the model
+    movieTitle = str(input("Enter a movie (example: Heat (1995) ): "))
+    recommendedMovie = recommendationSystemTest(movieTitle, k=10)
+    print("Top 10 Similar movies: ")
+    for movie, score in recommendedMovie:
+        print(f"Movie {movie} (sim={score:.3f})")
