621e698ffe105597dc50beacb5759bf36231ea6f
JohnnyBoiTime <chanky789@icloud.com>
2025-07-31 20:44:28 -0700

    Removu

--- movie_rec_ui/src/app/layout.tsx
+++ movie_rec_ui/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import StoreProvider from "./redux/StoreProvider";
-import { SessionProvider } from "next-auth/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
