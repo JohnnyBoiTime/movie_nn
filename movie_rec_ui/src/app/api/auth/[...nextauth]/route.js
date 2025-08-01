f1d5377feb93350d664a518ed4917d4a46d3bea1
JohnnyBoiTime <chanky789@icloud.com>
2025-07-31 20:15:59 -0700

    Changed something

--- movie_rec_ui/src/app/api/auth/[...nextauth]/route.js
+++ movie_rec_ui/src/app/api/auth/[...nextauth]/route.js
@@ -44,18 +44,9 @@ export const authOptions = {
                 return null;
             }
         }),
-
-        // Maybe implement this some other time
-        /*
-        GoogleProvider({
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET_KEY
-        }),
-        */
     ],
     secret: process.env.NEXTAUTH_SECRET_KEY,
 };
 
 // Send it to the project
-const handler =  NextAuth(authOptions);
-export {handler as GET, handler as POST};
+export default NextAuth(authOptions);
