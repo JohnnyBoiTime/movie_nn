a35deb3114383097e4b983dd7c6e4fa868aa61dd
JohnnyBoiTime <chanky789@icloud.com>
2025-07-31 21:41:24 -0700

    Huh

--- movie_rec_ui/src/app/apiRoutes/djangoAPI.ts
+++ movie_rec_ui/src/app/apiRoutes/djangoAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const djangoRoute = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_DJANGO_API_ROUTE, // For production
+  baseURL: `${process.env.NEXT_PUBLIC_DJANGO_API_ROUTE}`, // For production
   // baseURL: process.env.NEXT_PUBLIC_DJANGO_API_ROUTE_TEST, // For testing
   withCredentials: true,
 })
