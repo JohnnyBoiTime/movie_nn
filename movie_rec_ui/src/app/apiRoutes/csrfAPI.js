af93260ca670f1b3070d941571e3e03c780605c1
JohnnyBoiTime <chanky789@icloud.com>
2025-07-31 22:53:24 -0700

    ok

--- movie_rec_ui/src/app/apiRoutes/csrfAPI.js
+++ movie_rec_ui/src/app/apiRoutes/csrfAPI.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const API_BASE = process.env.NEXT_PUBLIC_BASE_ROUTE;
 
@@ -7,15 +8,13 @@ const preCSRFF = axios.create({
     baseURL: `${API_BASE}/api`, // For production
     // baseURL: '/api', // For testing
     withCredentials: true,
-    
 });
-
 
 
 // Axios for csrf 
 const csrfRoute = axios.create({
-    baseURL: `${API_BASE}/api`,
-    //baseURL: '/api', // For testing
+    //baseURL: `${API_BASE}/api`,
+    baseURL: `${API_BASE}/api`, // For testing
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
@@ -26,11 +25,14 @@ const csrfRoute = axios.create({
 
 // Cookeis + csrf token
 csrfRoute.interceptors.request.use(async (config) => {
-    if (!document.cookie.includes('csrftoken=')) {
-    await csrfRoute.get('/csrf/'); 
-    console.log('CSRF token fetched');
-  }
-  return config;
+    if (!Cookies.get('csrftoken')) {
+        await preCSRFF.get('/csrf/');
+
+        console.log("CSRF token fetched and set in cookies.");
+    }
+
+    config.headers['X-CSRFToken'] = Cookies.get('csrftoken');
+    return config;
 })
 
 
