082e26257c8b7f46d6e530baa8aa75382b450c2c
JohnnyBoiTime <chanky789@icloud.com>
2025-07-31 22:42:33 -0700

    CSRF

--- movie_rec_ui/src/app/apiRoutes/csrfAPI.js
+++ movie_rec_ui/src/app/apiRoutes/csrfAPI.js
@@ -3,12 +3,16 @@ import Cookies from 'js-cookie';
 
 const API_BASE = process.env.NEXT_PUBLIC_BASE_ROUTE;
 
+/*
 // Axios for pre-csrf
 const preCSRFF = axios.create({
     baseURL: `${API_BASE}/api`, // For production
     // baseURL: '/api', // For testing
     withCredentials: true,
+    
 });
+
+**/
 
 
 // Axios for csrf 
@@ -19,18 +23,17 @@ const csrfRoute = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
 });
 
 // Cookeis + csrf token
 csrfRoute.interceptors.request.use(async (config) => {
-    if (!Cookies.get('csrftoken')) {
-        await preCSRFF.get('/csrf/');
-
-        console.log("CSRF token fetched and set in cookies.");
-    }
-
-    config.headers['X-CSRFToken'] = Cookies.get('csrftoken');
-    return config;
+    if (!document.cookie.includes('csrftoken=')) {
+    await csrfRoute.get('/csrf/'); 
+    console.log('CSRF token fetched');
+  }
+  return config;
 })
 
 
