5499a6d733ed377b55075faf48dd12bdeea014c7
JohnnyBoiTime <chanky789@icloud.com>
2025-09-02 11:16:15 -0700

    Made it next/navigation for router

--- movie_rec_ui/src/app/savedMovies/page.tsx
+++ movie_rec_ui/src/app/savedMovies/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation";
 import { useGetSavedMoviesQuery } from '../redux/slices/savedMoviesSlice';
 
 export default function SavedMovies() {
