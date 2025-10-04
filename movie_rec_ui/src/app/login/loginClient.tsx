dc4c8fcc6cdd5cd5417e54c888b05c9cb0bb2fba
JohnnyBoiTime <chanky789@icloud.com>
2025-10-04 12:53:20 -0700

    Yeah

--- movie_rec_ui/src/app/login/loginClient.tsx
+++ movie_rec_ui/src/app/login/loginClient.tsx
@@ -53,6 +53,8 @@ export default function LoginPage() {
             <div>
                 <h1 className="text-center text-4xl font-extrabol mb-8">Welcome to movieNN!</h1>
                 <h2 className="text-center text-xl mb-8"> Neural-network powered movie recommendation system.</h2>
+                <h2 className="text-center text-xl mb-8"> Backend instance activates "per request", so may take a while on first recommendation</h2>
+                <h2 className="text-center text-xl mb-8"> Since instance is on a cold start and needs to warm up</h2>
             </div>
             <div className="h-screen w-full flex items-center justify-center px-4">
                 <div className="w-full max-w-sm  p-8 rounded-2xl shadow-lg border">
